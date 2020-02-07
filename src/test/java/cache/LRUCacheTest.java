c9ecdfda34604055d24bd4e6fb601792dc517a96
Nikolai Shalaev <nshalaev@wiley.com>
2020-02-07 15:32:43 +0300

    refactoring LRU tests

--- src/test/java/cache/LRUCacheTest.java
+++ src/test/java/cache/LRUCacheTest.java
@@ -5,25 +5,24 @@ import cache.factory.EvictionStrategy;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
 public class LRUCacheTest {
 
-    CacheFactory<String, Integer> cacheFactory;
     Cache<String, Integer> cache;
 
     @Before
     public void init(){
-        cacheFactory = new CacheFactory<>();
+        CacheFactory<String, Integer> cacheFactory = new CacheFactory<>();
         cache = cacheFactory.createCache(3, EvictionStrategy.LRU);
-
         cache.put("key1", 1);
         cache.put("key2", 2);
         cache.put("key3", 3);
-        cache.put("key1", 1);
+        cache.get("key1");
         cache.put("key4", 4);
+        cache.get("key1");
         cache.put("key5", 5);
+        cache.put("key6", 6);
     }
 
 
@@ -31,17 +30,25 @@ public class LRUCacheTest {
     public void receiveNull() {
         assertNull(cache.get("key2"));
         assertNull(cache.get("key3"));
+        assertNull(cache.get("key4"));
     }
 
     @Test
-    public void receiveLast() {
-        assertEquals(Integer.valueOf(5), cache.get("key5"));
+    public void receiveNotNull() {
+        assertNotNull(cache.get("key1"));
+        assertNotNull(cache.get("key5"));
+        assertNotNull(cache.get("key6"));
     }
 
     @Test
     public void removeElement() {
         cache.remove("key5");
         assertNull(cache.get("key5"));
+    }
+
+    @Test
+    public void getSize() {
+        assertEquals(3, cache.size());
     }
 
 }
